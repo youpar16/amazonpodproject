98515ae475fde358a9a1ada6d9e2e1a4e2245399
Andrew Park <andrewympark@gmail.com>
2022-09-22 15:34:49 -0400

    bg change

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
-import React, { useState } from 'react' 
-import './App.scss';
-import { Routes, Route } from 'react-router-dom'
-import UserDashboard from './components/UserDashboard/UserDashboard';
-import NewRequest from './components/NewRequest/NewRequest';
-import SignIn from './components/SignIn/SignIn';
-import NewUserDashboard from './components/NewUserDashboard/NewUserDashboard';
-import SubmittedRequest from './components/SubmittedRequest/SubmittedRequest';
+import React, { useState } from "react";
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import UserDashboard from "./components/UserDashboard/UserDashboard";
+import NewRequest from "./components/NewRequest/NewRequest";
+import SignIn from "./components/SignIn/SignIn";
+import NewUserDashboard from "./components/NewUserDashboard/NewUserDashboard";
+import SubmittedRequest from "./components/SubmittedRequest/SubmittedRequest";
 
 function App() {
-
-  const [bg, setBg] = useState(localStorage.getItem('bg-state') === 'false')
+  const [bg, setBg] = useState(localStorage.getItem("bg-state") === "true");
 
   React.useEffect(() => {
-    localStorage.setItem('bg-state', bg);
-  }, [bg])
+    localStorage.setItem("bg-state", bg);
+  }, [bg]);
 
   return (
     <>
       <div className="App">
         <Routes>
-          <Route exact path = '/' element = {<SignIn setBg={setBg}/>} />
-          <Route exact path = '/userdashboard' element = {<UserDashboard setBg={setBg}/>} />
-          <Route exact path = '/newrequest' element = {<NewRequest setBg={setBg}/>} />
-          <Route exact path = '/newuserdashboard' element = {<NewUserDashboard setBg={setBg}/>} />
-          <Route exact path = '/submittedrequest' element = {<SubmittedRequest setBg={setBg}/>} />
+          <Route exact path="/" element={<SignIn setBg={setBg} />} />
+          <Route
+            exact
+            path="/userdashboard"
+            element={<UserDashboard setBg={setBg} />}
+          />
+          <Route
+            exact
+            path="/newrequest"
+            element={<NewRequest setBg={setBg} />}
+          />
+          <Route
+            exact
+            path="/newuserdashboard"
+            element={<NewUserDashboard setBg={setBg} />}
+          />
+          <Route
+            exact
+            path="/submittedrequest"
+            element={<SubmittedRequest setBg={setBg} />}
+          />
         </Routes>
       </div>
-      <div className='bg' data-show={bg}/>
+      <div className="bg" data-show={bg} />
     </>
   );
 }
